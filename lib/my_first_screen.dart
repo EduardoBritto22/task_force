2bdef5aa32425aea57a044d42899dc715aa6bcac
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-28 12:19:26 +0100

    First screen implementation with App bar and FAB

---
+++ lib/my_first_screen.dart
@@ -0,0 +1,71 @@
+// Container(
+// color: Colors.white,
+// child: Column(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// color: Colors.amber,
+// width: 100,
+// height: 100,
+// ),
+// Container(
+// color: Colors.cyan,
+// width: 50,
+// height: 50,
+// )
+// ],
+// ),
+// Stack(
+// alignment: AlignmentDirectional.center,
+// children: [
+// Container(
+// color: Colors.cyan,
+// width: 100,
+// height: 100,
+// ),
+// Container(
+// color: Colors.amber,
+// width: 50,
+// height: 50,
+// )
+// ],
+// ),
+// Row(
+// mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// crossAxisAlignment: CrossAxisAlignment.center,
+// children: [
+// Container(
+// color: Colors.red,
+// width: 50,
+// height: 50,
+// ),
+// Container(
+// color: Colors.pinkAccent,
+// width: 50,
+// height: 50,
+// ),
+// Container(
+// color: Colors.blueAccent,
+// height: 50,
+// width: 50,
+// )
+// ],
+// ),
+// Container(
+// color: Colors.deepOrange,
+// height: 30,
+// width: 300,
+// child: Text(
+// 'Diamante laranja',
+// style: TextStyle(color: Colors.black, fontSize: 28),
+// textAlign: TextAlign.center,
+// ),
+// ),
+// ElevatedButton(onPressed: () {}, child: Text('Aperte o botão!'))
+// ],
+// ),
+// )
