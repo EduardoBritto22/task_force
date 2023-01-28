02471ee2d37c4660c87171b72f3067f8344144d7
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-28 12:50:39 +0100

    Add Widget for a Task card

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,78 @@ class MyApp extends StatelessWidget {
         ),
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter my first steps'),
+            title: Text('Tasks'),
           ),
-          body: Container(),
-          floatingActionButton: FloatingActionButton(onPressed: (){}),
+          body: ListView(
+            children: [
+              Task('Leard Flutter'),
+              Task('Ride a bike'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+              Task('Meditate'),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(onPressed: () {}),
         ));
+  }
+}
+
+class Task extends StatelessWidget {
+  final String name;
+
+  const Task(this.name, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 140,
+            ),
+            Container(
+                color: Colors.white,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      color: Colors.black26,
+                      width: 72,
+                      height: 100,
+                    ),
+                    Container( // To constrain the size of the text
+                      width: 200,
+                      child: Text(
+                        name,
+                        style: TextStyle(fontSize: 24),
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
