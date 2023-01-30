7ca26c3bb191fd18c73ef1d8fe583adc7345b7d4
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-30 10:41:23 +0100

    - Add the form fields for name, difficulty and image

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -8,6 +8,10 @@ class FormScreen extends StatefulWidget {
 }
 
 class _FormScreenState extends State<FormScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController difficultyController = TextEditingController();
+  TextEditingController imageController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,7 +28,62 @@ class _FormScreenState extends State<FormScreen> {
               border: Border.all(width: 3)),
           child: Column(
             children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  controller: nameController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Name',
+                      fillColor: Colors.white70,
+                      filled: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  textAlign: TextAlign.center,
+                  controller: difficultyController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Difficulty',
+                      fillColor: Colors.white70,
+                      filled: true),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  onChanged: (text){
+                    setState(() {
 
+                    });
+                  },
+                  textAlign: TextAlign.center,
+                  controller: imageController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Image',
+                      fillColor: Colors.white70,
+                      filled: true),
+                ),
+              ),
+              Container(
+                height: 100,
+                width: 72,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 2, color: Colors.blue)
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(imageController.text, fit: BoxFit.cover),
+
+                ),
+              ),
+              ElevatedButton(onPressed: () {}, child: Text('Add !'))
             ],
           ),
         ),
