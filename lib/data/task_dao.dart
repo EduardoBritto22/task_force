5014497d5d4c3a57d87b2f9175bb34cf552964d1
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-31 17:30:40 +0100

    Add CRUD functions to read the tasks from the database

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
+import 'package:sqflite/sqflite.dart';
 import 'package:task_force/components/task.dart';
+import 'package:task_force/data/database.dart';
 
 class TaskDao {
   static const String _tablename = 'taskTable';
@@ -14,9 +16,27 @@ class TaskDao {
 
   save(Task task) async {}
 
-  Future<List<Task>> findAll() async {}
+  Future<List<Task>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tablename);
+    return toList(result);
+  }
 
-  Future<List<Task>> find(String taskName) async {}
+  Future<List<Task>> find(String taskName) async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tablename, where: '$_name = ?',whereArgs: [taskName]);
+    return toList(result);
+  }
 
   delete(String taskName) async {}
+
+  List<Task> toList(List<Map<String, dynamic>> taskMap) {
+    final List<Task> tasks = List.empty();
+    for (var row in taskMap) {
+      final Task task = Task(row[_name], row[_image], row[_difficulty]);
+      tasks.add(task);
+    }
+
+    return tasks;
+  }
 }
